ad8d38a93c493291a5bfe7766fce546573a5363b
LukeWarmACCT <lukewarmgitacct@gmail.com>
2017-10-01 21:04:20 -0400

    Fixed button alignment problem

--- index.php
+++ index.php
@@ -39,69 +39,71 @@
       </div>
 
       <div class='row'>
-        <div class='col-md-6'>
-          <!-- This was the text I got from my friend that inspired this stupid webpage -->
-          <p>I was talking to a guy the other day</p>
-          <p>He's from Burma</p>
-          <p>And he was telling me he has an arranged marriage</p>
-          <p>Or is in the process of it</p>
-          <p>So not married yet, but his parents have picked out a top bitch for him to marry</p>
-          <p>And so now he's in school so he can make enough money to purchase 3 cows</p>
-          <p>Because that is the price that the girls parents placed on their daughter</p>
-          <p>He comes from a culture with a farm based economy</p>
-          <p>So the top bitches are worth cows</p>
-          <br>
+        <div class = 'col-md-12'>
+          <div class='col-md-6'>
+            <!-- This was the text I got from my friend that inspired this stupid webpage -->
+            <p>I was talking to a guy the other day</p>
+            <p>He's from Burma</p>
+            <p>And he was telling me he has an arranged marriage</p>
+            <p>Or is in the process of it</p>
+            <p>So not married yet, but his parents have picked out a top bitch for him to marry</p>
+            <p>And so now he's in school so he can make enough money to purchase 3 cows</p>
+            <p>Because that is the price that the girls parents placed on their daughter</p>
+            <p>He comes from a culture with a farm based economy</p>
+            <p>So the top bitches are worth cows</p>
+            <br>
+          </div>
+
+          <div class='col-md-6'> <!-- form contents excluding submit -->
+            <form name='burma_form' id='burma_form' method='POST'> <!--  action='process.php' ?? -->
+              <div class='col-md-6'> <!-- left form row -->
+
+                <div class='form-group'>
+                  <label for='burma_form'>Religion</label>
+                  <select name='religion' class='form-control' id='religion'>
+                    <option value='christianity'>Christianity</option>
+                    <option value='atheism'>Atheism</option>
+                    <option value='hinduism'>Hinduism</option>
+                  </select>
+                </div>
+
+                <div class='form-group'>
+                  <label for='burma_form'>Education</label>
+                  <select name='education' class='form-control' id='education'>
+                    <option value='hs'>High school</option>
+                    <option value='bs_d'>Bachlors Degree</option>
+                    <option value='ms_d'>Masters Degree</option>
+                  </select>
+                </div>
+
+              </div> <!-- left form row -->
+
+              <div class='col-md-6'> <!-- right form row -->
+
+                <label for='burma_form'>Attractiveness</label>
+                <div class='form-inline' id='attr'>
+                  1 <input type='radio' name='radio_attr' value='1' checked='checked'>
+                  2 <input type='radio' name='radio_attr' value='2'>
+                  3 <input type='radio' name='radio_attr' value='3'>
+                  4 <input type='radio' name='radio_attr' value='4'>
+                  5 <input type='radio' name='radio_attr' value='5'>
+                </div>
+
+              </div> <!-- right form row -->
+
+            </div> <!-- form contents excluding submit -->
+
+            <div class='col-md-6'></div>
+            <div class='col-md-6'> <!-- submit and results -->
+              <br>
+              <button class='btn btn-primary btn-block' id='submit'>Submit</button>
+              <br>
+              <h3 id='daughter_heading'></h3>
+              <p id='daughter'></p>
+            </div> <!-- submit and results -->
+
+          </form>
         </div>
-
-        <div class='col-md-6'> <!-- form contents excluding submit -->
-          <form name='burma_form' id='burma_form' method='POST'> <!--  action='process.php' ?? -->
-            <div class='col-md-6'> <!-- left form row -->
-
-              <div class='form-group'>
-                <label for='burma_form'>Religion</label>
-                <select name='religion' class='form-control' id='religion'>
-                  <option value='christianity'>Christianity</option>
-                  <option value='atheism'>Atheism</option>
-                  <option value='hinduism'>Hinduism</option>
-                </select>
-              </div>
-
-              <div class='form-group'>
-                <label for='burma_form'>Education</label>
-                <select name='education' class='form-control' id='education'>
-                  <option value='hs'>High school</option>
-                  <option value='bs_d'>Bachlors Degree</option>
-                  <option value='ms_d'>Masters Degree</option>
-                </select>
-              </div>
-
-            </div> <!-- left form row -->
-
-            <div class='col-md-6'> <!-- right form row -->
-
-              <label for='burma_form'>Attractiveness</label>
-              <div class='form-inline' id='attr'>
-                1 <input type='radio' name='radio_attr' value='1' checked='checked'>
-                2 <input type='radio' name='radio_attr' value='2'>
-                3 <input type='radio' name='radio_attr' value='3'>
-                4 <input type='radio' name='radio_attr' value='4'>
-                5 <input type='radio' name='radio_attr' value='5'>
-              </div>
-
-            </div> <!-- right form row -->
-
-          </div> <!-- form contents excluding submit -->
-
-          <div class='col-md-6 pull-right'> <!-- submit and results -->
-            <br>
-            <button class='btn btn-primary btn-block' id='submit'>Submit</button>
-            <br>
-            <h3 id='daughter_heading'></h3>
-            <p id='daughter'></p>
-          </div> <!-- submit and results -->
-          <div class="clearfix"></div>
-
-        </form>
 
       </div> <!-- row -->
 
