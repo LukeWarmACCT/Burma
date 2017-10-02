05948e27a52b83bdbdc1a7e8d37b1d07b92754ad
Kevin Raoofi <nullindev@gmail.com>
2017-10-01 23:45:05 -0400

    Use a separate service for best practices

--- burmaApp.js
+++ burmaApp.js
@@ -1,13 +1,6 @@
 (function(){
-  const educationMap = {
-    'bs_d': 2000,
-    'ms_d': 2000,
-    'hs': 140,
-    'hs_drop': 30
-  };
-
   class BurmaController {
-    constructor() {
+    constructor(calc) {
       this.education = 'hs';
       this.religion = 'christianity';
       this.attractive = '1';
@@ -15,49 +8,25 @@
       this.submitted = false;
       this.currency = null;
       this.results = null;
+
+      this.calc = calc;
     }
 
     update() {
-      let value = 0;
-
-      if(this.religion === 'hinduism') {
-        value += 2000;
-      }
-
-      if(educationMap[this.education]) {
-        value += educationMap[this.education];
-      }
-
-      value += this.attractive * 400;
-
-      let cows = 0;
-      let pigs = 0;
-      let chickens = 0;
-
-      cows = Math.floor(value / 2000);
-      if(cows >= 1) { value = value % 2000; }
-      pigs = Math.floor(value / 100);
-      if(pigs >= 1) { value = value % 100; }
-      chickens = Math.floor(value);
-
-      this.currency = {
-        cow: cows,
-        pig: pigs,
-        chicken: chickens
-      };
-    };
+      this.currency = this.calc.compute(this.religion, this.attractive, this.education);
+    }
 
     /**
     * Utility method to display plurals if necessary
     */
-    _economize(currency) {
+    pluralize(currency) {
       let num = this.currency[currency];
       if(num === 1) {
         return `${num} ${currency}`;
       } else {
         return `${num} ${currency}s`;
       }
-    };
+    }
 
     /**
     * Computes results for template
@@ -80,6 +49,46 @@
     };
   }
 
+  class BurmaCalculationService {
+    compute(religion, attractive, education) {
+      let value = 0;
+
+      if(religion === 'hinduism') {
+        value += 2000;
+      }
+
+      if(BurmaCalculationService.educationMap[education]) {
+        value += BurmaCalculationService.educationMap[education];
+      }
+
+      value += attractive * 400;
+
+      let cows = 0;
+      let pigs = 0;
+      let chickens = 0;
+
+      cows = Math.floor(value / 2000);
+      if(cows >= 1) { value = value % 2000; }
+      pigs = Math.floor(value / 100);
+      if(pigs >= 1) { value = value % 100; }
+      chickens = Math.floor(value);
+
+      return  {
+        cow: cows,
+        pig: pigs,
+        chicken: chickens
+      };
+    }
+  }
+
+  BurmaCalculationService.educationMap = {
+    'bs_d': 2000,
+    'ms_d': 2000,
+    'hs': 140,
+    'hs_drop': 30
+  };
+
   angular.module('burmaApp', [])
-    .controller('BurmaController', [BurmaController]);
+    .service('BurmaCalculationService', BurmaCalculationService)
+    .controller('BurmaController', ['BurmaCalculationService', BurmaController]);
 })();
